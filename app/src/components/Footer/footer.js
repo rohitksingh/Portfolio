baee91a42da28ddd4e74821f63fd3b9f1558ec3f
Rohit <r4rohit002@gmail.com>
2021-02-27 15:30:57 -0700

    Created Navabar

---
+++ app/src/components/Footer/footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Footer(){
+
+    return (
+        <div>
+            <div className="container">
+                Created by Rohit Singh
+            </div>
+        </div>
+
+        
+    );
+
+} 
+
+export default Footer
