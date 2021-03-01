123de5485ec43277c6b9bcaaaf56f43a71015c53
Rohit <r4rohit002@gmail.com>
2021-02-28 20:34:43 -0700

    Added image in jumbotron

--- app/src/components/OtherProjects/otherProjects.js
+++ app/src/components/OtherProjects/otherProjects.js
@@ -1,6 +1,7 @@
-import React from 'react-bootstrap'
+import React, { Container, Jumbotron, Image } from 'react-bootstrap'
 import ProjectCard from '../ProjectCard/projectcard'
 import projectData from  '../../static/otherprojects.json'
+import '../OtherProjects/otherProjects.css'
 
 
 function loaddata() {  
@@ -13,10 +14,31 @@ function OtherProjects(){
 
     return (
         <div>
-            Other projects
+            {getJumbotron()} 
             {loaddata()}
         </div>
     );
+}
+
+function getProjectImages(){
+    return projectData.map((data) => {
+        return <Image className="project-image" src={data.imgUrl} />
+    })
+}
+
+function getJumbotron() {
+    return (
+        <Jumbotron className="project-jumbotron">
+            <Container>
+                <h1 className="jumbotron-title display-1">Other Projects</h1>
+                <div id="skills" class="skills">
+                    <div class="skill-name">
+                        {getProjectImages()}
+                    </div>
+                </div>        
+            </Container>
+        </Jumbotron>
+    )
 }
 
 export default OtherProjects;
